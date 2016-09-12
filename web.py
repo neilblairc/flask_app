aac8ae0827c3a021bb79b7fe6f9a40082b9ecf8c
nchristensen admin <nchristensenadmin@nchristensen-air.local>
2016-09-11 23:58:59 -0700

    update

--- web.py
+++ web.py
@@ -8,7 +8,7 @@ def index():
 	address = request.values.get('address')
 	businesses = None
 	if address:
-		businesses = yelp_api.get_businesses('address')
+		businesses = yelp_api.get_businesses(location, term)
 	return render_template('index.html', businesses=businesses)
 
 @app.route("/about")
